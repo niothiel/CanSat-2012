4ab69b835f03c2e52ca60b7bfa3c7ab7d8f61d39
Val Kom <niothiel@gmail.com>
2012-04-19 17:25:39 -0400

    Added Buzzer and Servo code. Just the very basics as they currently can't be run without external power.

---
+++ Buzzer.ino
@@ -0,0 +1,14 @@
+#define BUZZER_PIN 10
+#define FREQUENCY 2000
+
+void initBuzzer() {
+  //pinMode(BUZZER_PIN, OUTPUT);
+}
+
+void turnBuzzerOn() {
+  tone(BUZZER_PIN, FREQUENCY);
+}
+
+void turnBuzzerOff() {
+  noTone(BUZZER_PIN);
+}
---
+++ Servo.ino
@@ -0,0 +1,27 @@
+#include <Servo.h>
+
+#define SERVO_PIN 9
+
+// Positions that we want the servos to be.
+// It's #define'd for quick changing.
+// Valid values are [0,180] inclusive.
+#define POS1 0
+#define POS2 180
+
+Servo servo;
+
+void initServo() {
+  servo.attach(SERVO_PIN);
+}
+
+void servoPos1() {
+  servo.write(POS1);
+  delay(50);
+}
+
+void servoPos2() {
+  servo.write(POS2);
+  delay(50);
+}
+
+
