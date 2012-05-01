341569c2b760bc95101ac5315ddacb4722fda114
Val Kom <niothiel@gmail.com>
2012-05-01 00:23:02 -0400

    Lots of changes to EEPROM
    Implemented test and dump functions (working)
    Added a different way of logging packets
    	Using byte-write turned out too slow
    	so now we need to use page-write
    	to write the data, but as of now
    	the code to do that is bugged
    	and not complete.
    Other improvements, check the diffs.

--- Eeprom.ino
+++ Eeprom.ino
@@ -16,6 +16,9 @@ void i2c_eeprom_write_byte( int deviceaddress, unsigned int eeaddress, byte data
   Wire.write((int)(eeaddress & 0xFF)); // LSB
   Wire.write(rdata);
   Wire.endTransmission();
+  
+  // Need to add a delay as random writes are way too slow on this EEPROM.
+  delay(5);
 }
 
 // WARNING: address is a page address, 6-bit end will wrap around
@@ -25,8 +28,9 @@ void i2c_eeprom_write_page( int deviceaddress, unsigned int eeaddresspage, byte*
   Wire.write((int)(eeaddresspage >> 8)); // MSB
   Wire.write((int)(eeaddresspage & 0xFF)); // LSB
   byte c;
-  for ( c = 0; c < length; c++)
+  for ( c = 0; c < length; c++) {
     Wire.write(data[c]);
+  }
   Wire.endTransmission();
 }
 
@@ -56,47 +60,65 @@ void i2c_eeprom_read_buffer( int deviceaddress, unsigned int eeaddress, byte *bu
 
 void initEeprom() 
 {
-  char somedata[] = "this is data from the eeprom"; // data to write
+  /*char somedata[] = "this is data from the eeprom"; // data to write
   i2c_eeprom_write_page(0x50, 0, (byte *)somedata, sizeof(somedata)); // write to EEPROM 
 
-  delay(10); //add a small delay
-
-  Serial.println("Memory written");
+  delay(10); //add a small delay*/
 }
 
 // Logs the current telemetry packet to the EEPROM.
 // Keeps track of the current address
 void logPacket() {
-  return;
-  
-  Serial.println("Logging packet to EEPROM...");
+  unsigned long loggingTime = millis();
+  Serial.println("Logging Packet to EEPROM");
   
   for(int x = 0; x < strlen(packetBuffer); x++) {
-    Serial.print("Data: ");
-    Serial.print( (char)packetBuffer[x]);
-    Serial.print("    Addr: ");
-    Serial.println(currentEEAddress);
-    
-//    i2c_eeprom_write_byte( 0x50, currentEEAddress, (byte) packetBuffer[x]);
-//    delay(1);
-
+    i2c_eeprom_write_byte( 0x50, currentEEAddress, (byte) packetBuffer[x]);
     currentEEAddress++;
   }
   
-  Serial.println("Logging finished...");
+  // Write a NULL at the end so that we know when the dump is finished.
+  i2c_eeprom_write_byte( 0x50, currentEEAddress, (byte) 0x00);
+  
+  Serial.print("Logging Done");
+  Serial.println(millis() - loggingTime);
+}
+
+// Test method for logging packets using page writes..
+// Using the byte write method is almost certainly going to be too slow for our purposes
+// The function is currently broken and should not be used.
+void logPacket2() {
+  unsigned long loggingTime = millis();
+  
+  byte offset = 0;
+  while(offset < strlen(packetBuffer)) {
+    byte len = strlen(packetBuffer) - offset > 30 ? 30 : strlen(packetBuffer) - offset;
+    
+    i2c_eeprom_write_page(0x50, currentEEAddress, (byte*)(packetBuffer + offset), len);
+    
+    offset += len;
+    currentEEAddress += len;
+    
+    Serial.print("Len: ");
+    Serial.println(len);
+    Serial.print("Offset: ");
+    Serial.println(offset);
+    Serial.print("EEADDR: ");
+    Serial.println(currentEEAddress);
+    delay(25);
+  }
+  
+  // Write a NULL at the end so that we know when the dump is finished.
+  i2c_eeprom_write_byte( 0x50, currentEEAddress, (byte) 0x00);
+  
+  Serial.print("Logging Done");
+  Serial.println(millis() - loggingTime);
 }
 
 void dumpEeprom() {
-  Serial.println("Dumping Eeprom values");
-}
-
-void testEeprom() {
-  Serial.println("Testing Eeprom");
-}
-
-void readEeprom() 
-{
-  int addr=0; //first address
+  Serial.println("Eeprom Dump:");
+  
+  unsigned int addr = 0; //first address
   byte b = i2c_eeprom_read_byte(0x50, 0); // access the first address from the memory
 
   while (b!=0) 
@@ -105,6 +127,24 @@ void readEeprom()
     addr++; //increase address
     b = i2c_eeprom_read_byte(0x50, addr); //access an address from the memory
   }
-  Serial.println(" ");
-  delay(2000);
+  Serial.println("\nDump Done");
+}
+
+void testEeprom() {
+  Serial.println("Testing Eeprom");
+  
+  //char testString[] = "Test Successful.";
+  //i2c_eeprom_write_page(0x50, 0, (byte*)testString, sizeof(testString));
+  
+  i2c_eeprom_write_byte(0x50, 0, (byte)'a');
+  i2c_eeprom_write_byte(0x50, 1, (byte)'b');
+  i2c_eeprom_write_byte(0x50, 2, (byte)'c');
+  i2c_eeprom_write_byte(0x50, 3, (byte)'d');
+  i2c_eeprom_write_byte(0x50, 4, (byte)'e');
+  i2c_eeprom_write_byte(0x50, 5, (byte)'f');
+  i2c_eeprom_write_byte(0x50, 6, (byte)'g');
+  i2c_eeprom_write_byte(0x50, 7, (byte)'h');
+  i2c_eeprom_write_byte(0x50, 8, (byte)0x00);
+  
+  dumpEeprom();
 }
